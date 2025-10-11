3ecea5162f5aca2c0f2536cbc00bac945c68444e
Eccentricdiva <osinderotemilola1@gmail.com>
2025-10-11 13:02:52 +0100

    Fix Post model: use content field instead of body

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-
-    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
 
