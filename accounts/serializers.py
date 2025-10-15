9a3c704731ed288c5f861c1911cf3422670b2a72
Eccentricdiva <osinderotemilola1@gmail.com>
2025-10-15 10:30:34 +0100

    Fix: use get_user_model in UserRegistrationSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,20 +3,18 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class RegisterSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture', 'followers']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(   # 👈 EXACT LINE NEEDED
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=validated_data.get('email'),
             password=validated_data['password']
         )
         user.bio = validated_data.get('bio', '')
         user.profile_picture = validated_data.get('profile_picture', None)
-        user.save()
-        return user
