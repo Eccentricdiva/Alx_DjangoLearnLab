009cd5b463b27bb770b0567898110d2c29cccde0
Eccentricdiva <osinderotemilola1@gmail.com>
2025-10-18 16:40:12 +0100

    updated views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required   # ALX checker expects this
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
@@ -11,7 +12,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    ordering = ['-created_at']  # show newest first
+    ordering = ['-created_at']
     paginate_by = 10
 
 
@@ -44,7 +45,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post-list')  # FIXED name (singular)
+    success_url = reverse_lazy('post-list')
 
     def test_func(self):
         post = self.get_object()
