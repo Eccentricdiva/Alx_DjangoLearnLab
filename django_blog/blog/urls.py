286d8ff7f5ea2ef5463fa00fc625afe53a6c4922
Eccentricdiva <osinderotemilola1@gmail.com>
2025-10-18 16:35:35 +0100

    updated crud

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,10 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('posts/', PostListView.as_view(), name='posts-list'),
-    path('posts/new/', PostCreateView.as_view(), name='posts-create'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='posts-detail'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='posts-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='posts-delete'),
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
-
