6ceaecfac0325504ea28b1ee607710a5b288ad42
Eccentricdiva <osinderotemilola1@gmail.com>
2025-10-19 15:40:21 +0100

    Implement accounts app with registration, login, and token

--- social_media_api/social_media_api/manage.py
+++ social_media_api/social_media_api/manage.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-#!/usr/bin/env python
 import os
 import sys
+
 
 def main():
     """Run administrative tasks."""
